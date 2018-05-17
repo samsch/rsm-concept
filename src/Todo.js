e3e58165784a826a936ee7c64c7e113c9bff1d51
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2018-05-17 17:28:56 -0400

    Added commentary to Todo example

--- src/Todo.js
+++ src/Todo.js
@@ -5,12 +5,26 @@ import { makeStateComponent } from './rsm';
 let id = 0;
 const actions = {
   updateNewTodoValue: value => state => ({...state, newTodoValue: value}),
-  addTodo: () => state =>
-    R.over(R.lensPath(['list']), list => list.concat({ desc: state.newTodoValue, completed: false, id: `t-${id++}` }), state),
+  addTodo: () => state => ({
+    ...state,
+    list: state.list.concat({ desc: state.newTodoValue, completed: false, id: `t-${id++}` }),
+    // newTodoValue could be cleared here, but instead we are calling updateNewTodoValue from
+    // the submit handler below to show how multiple actions called will only result in one update.
+  }),
+  // A more functional update style can be used with something like Ramda, as shown in these next two
+  // actions. These two in particular could probably be simplified more using partial.lenses as well.
   completeTodo: id => state =>
-    R.set(R.lensPath(['list', state.list.findIndex(todoItem => todoItem.id === id), 'completed']), true, state),
+    R.set(
+      R.lensPath(['list', state.list.findIndex(todoItem => todoItem.id === id), 'completed']),
+      true,
+      state
+    ),
   uncompleteTodoClone: id => state =>
-    R.set(R.lensPath(['list', state.list.findIndex(todoItem => todoItem.id === id), 'completed']), false, state),
+    R.set(
+      R.lensPath(['list', state.list.findIndex(todoItem => todoItem.id === id), 'completed']),
+      false,
+      state
+    ),
   uncompleteTodo: id => state => {
     const index = state.list.findIndex(todoItem => todoItem.id === id);
     return {
@@ -60,6 +74,11 @@ const Todo = () => (
           e.preventDefault();
           actions.addTodo();
           actions.updateNewTodoValue('');
+          // The result of these actions would normally be combined in addTodo(), however, calling
+          // multiple actions sychronously only results in a single update (and render) which is
+          // shown here. (Just add a console.log() call in this render function to see it only is
+          // called once on submit, and you will also only see one "Next State" log with rsm
+          // debugging turned on.)
         }}>
           <div className="form-group">
             <label htmlFor="add-todo-text">Todo item</label>
@@ -68,7 +87,7 @@ const Todo = () => (
               id="add-todo-text"
               className="form-control"
               value={state.newTodoValue}
-              onChange={({ target: { value } }) => actions.updateNewTodoValue(value)}
+              onChange={event => actions.updateNewTodoValue(event.target.value)}
             />
             <button type="submit" className="btn btn-outline-primary">Add Todo</button>
           </div>
